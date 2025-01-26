2602fe4eaf7e3988cd9965b73f25f466fcb7125b
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2025-01-26 18:09:48 +0100

    ** fix ** moved the api call to the backend

--- islands/Joke.tsx
+++ islands/Joke.tsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from "preact/hooks";
 
-interface Joke {
-  setup: string;
-  punchline: string;
-}
 
 export const Joke = () => {
-  const [joke, setJoke] = useState<Joke>({ setup: "", punchline: "" });
+  const [joke, setJoke] = useState("");
 
   const fetchNewJoke = async () => {
     const res = await fetch(
-      "https://official-joke-api.appspot.com/random_joke",
+      "/api/joke",
     );
     const newJoke = await res.json();
     setJoke(newJoke);
@@ -23,8 +19,7 @@ export const Joke = () => {
   return (
     <div class="p-4">
       <h1 class="text-xl font-bold">Random Joke</h1>
-      <p class="text-lg">{joke.setup}</p>
-      <p class="text-lg font-semibold mt-2">{joke.punchline}</p>
+      <p class="text-lg">{joke}</p>
       <button
         onClick={fetchNewJoke}
         class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
